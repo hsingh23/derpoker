05bb750a6fb79648c9c0132ec2e75ba7a7b04521
Kurtis Nusbaum <klnusbaum@gmail.com>
2012-10-31 13:55:03 -0500

    Added static stuff for serving. hopefully this works.

--- pokeviewer/urls.py
+++ pokeviewer/urls.py
@@ -2,3 +2,8 @@ from django.conf.urls import patterns, include, url
 urlpatterns = patterns('pokeviewer.views',
     url(r'^$', 'home'),
 )
+
+if not settings.DEBUG:
+  urlpatterns += patterns('',
+      (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+      )
