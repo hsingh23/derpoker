f960ffc579608c31dca93e6c290f3e7f27902968
Kurtis Nusbaum <klnusbaum@gmail.com>
2012-10-31 03:46:26 -0500

    added table of pokers, let's see if this works.

--- pokeviewer/views.py
+++ pokeviewer/views.py
@@ -1,9 +1,15 @@
+from pokeviewer.models import Poke
 from django.template import Context, RequestContext, loader
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render_to_response
+from django.db.models import Count
+
 
 def home(request):
+  pokers = Poke.objects.annotate(num_pokes=Count('poker'))
+
+
   return render_to_response(
     'home.html',
-    {'page' : 'home' },
+    {'pokers' : pokers },
     context_instance=RequestContext(request))
