b37941e367ff7718f3cd1dd811dfe734fcca99fa
Kurtis Nusbaum <klnusbaum@gmail.com>
2012-10-31 13:59:23 -0500

    ok, let's try this for static

--- pokeviewer/urls.py
+++ pokeviewer/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import patterns, include, url
+from derpoker import settings
 urlpatterns = patterns('pokeviewer.views',
     url(r'^$', 'home'),
 )
 
-urlpatterns += patterns(”,
-  (r’^static/(?P.*)$’, ‘django.views.static.serve’, {‘document_root’: settings.STATIC_ROOT}),
+urlpatterns += patterns('',
+  (r'^static/(?P.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
 )
