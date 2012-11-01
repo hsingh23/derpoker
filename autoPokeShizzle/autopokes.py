68fab0de7b704cd03eadffde198909a8106c67b9
Kurtis Nusbaum <klnusbaum@gmail.com>
2012-11-01 15:07:27 -0500

    Changed autopoking to be more conservative

--- autoPokeShizzle/autopokes.py
+++ autoPokeShizzle/autopokes.py
@@ -60,7 +60,7 @@ def loopThatShit(browser, logConn):
       pokeDashboard = browser.find_element_by_class_name('pokesDashboard')
       pokers = pokeDashboard.find_elements_by_class_name('objectListItem')
       for poker in pokers:
-        sleep(0.25 + random.uniform(0,0.5))
+        sleep(0.5 + random.uniform(0,0.5))
         try:
           pokeLink = poker.find_element_by_link_text('Poke Back')
           pokeheader = poker.find_element_by_class_name('pokeHeader')
@@ -71,7 +71,7 @@ def loopThatShit(browser, logConn):
         except Exception:
           pass
 
-      sleep(30 + random.uniform(-5,5))
+      sleep(60 + random.uniform(-10,10))
 
 print "Opening display"
 display = Display(visible=0, size=(800, 600))
