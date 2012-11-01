0b846c8b1b709afb7ace8760a2708bda7b31ff33
Kurtis Nusbaum <klnusbaum@gmail.com>
2012-11-01 17:47:11 -0500

    Added information about how we only show the top X pokers.

--- pokeviewer/views.py
+++ pokeviewer/views.py
@@ -30,5 +30,8 @@ def home(request):
 
   return render_to_response(
     'home.html',
-    {'pokers' : pokers },
+    {
+      'pokers' : pokers,
+      'max_pokers' : str(MAX_POKES_SHOW)
+    },
     context_instance=RequestContext(request))
